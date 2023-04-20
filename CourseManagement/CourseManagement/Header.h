eddfc1208d6c8e630a4051db5b1e1482246bd051
BLuBin7 <builuongbinh7@gmail.com>
2023-04-20 22:45:00 +0700

    add

--- CourseManagement/CourseManagement/Header.h
+++ CourseManagement/CourseManagement/Header.h
@@ -1,1 +1,85 @@
 #pragma once
+#include<iostream>
+using namespace std;
+class Student {
+private:
+	string name;
+	string id;
+
+public:
+	Student(string name, string id) : name(name), id(id){}
+	
+	string getName() {
+		return this->name;
+	}
+
+	void setName(string value){
+		this->name = value;
+	}
+
+	string getId() {
+		return this->id;
+	}
+
+	void setId(string value) {
+		this->id = value;
+	}
+
+
+};
+
+class Person {
+private:
+	string name;
+public:
+	 Person(string name): name(name){}
+
+	 string getName() {
+		 return this->name;
+	 }
+
+	 void setName(string value) {
+		 this->name = value;
+	 }
+
+};
+
+class Semester {
+private:
+	string name;
+public:
+	Semester(string name) : name(name){}
+
+	string getName() {
+		return this->name;
+	}
+	
+	void setName(string value) {
+		this->name = value;
+	}
+
+};
+
+class Course {
+private:
+	string name;
+	string id;
+
+public:
+	Course(string name, string id): name(name), id(id){}
+	string getName() {
+		return this->name;
+	}
+
+	void setName(string value) {
+		this->name = value;
+	}
+
+	string getId() {
+		return this->id;
+	}
+
+	void setId(string value) {
+		this->id = value;
+	}
+};
